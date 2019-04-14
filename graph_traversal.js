f0ad91d5a6390777ae545bffc38bdf2c21c2feb2
Robin Cristobal <racristob@gmail.com>
2019-04-13 21:29:51 -0700

    graph traversal finish, depth first recursive+iterative and breadthfirst iterative

--- graph_traversal.js
+++ graph_traversal.js
@@ -7,6 +7,8 @@ Graph Traversal:
 	shortest path/distance for GPS, mazes, and AI (shortest path to win game)
 - Trees are special subset of graphs; every tree is a graph; not all graphs are trees.
 
+Traversing : visiting every vertex in the graph  
+Must remember the visited nodes.
 
 */
 
@@ -66,4 +68,99 @@ class Graph {
 			}
 		}
 	}
+
+	// Depth First Traversal (Recursion), and Helper Method
+	depth_first_traversal_recursive(vertex) {
+		let ordered = [];
+		let visited = {};
+		this.dfs_t_helper(vertex, visited, ordered);
+		return ordered;
+	}
+	dfs_t_helper(vertex, visited, order) {
+		if (this.adjacencyList[vertex] === undefined) return visited;
+		visited[vertex] = true;
+		order.push(vertex);
+		for (let i = 0; i < this.adjacencyList[vertex].length; i++) {
+			let child = this.adjacencyList[vertex][i];
+			if (!visited[child]) { // if child hasn't been visited yet
+				this.dfs_t_helper(child,visited,order);
+			}
+		}
+		return visited; // this is the base case, when there is no more children to visit, it returns
+	}
+
+	// Depth First Traversal Iterative; USE A STACK!
+	depth_first_traversal_iterative(vertex) {
+		// we push the vertex into stack
+		// while stack is not empty
+		//		pop off the stack and mark as visited
+		//		push the popped off vertex's neighbors into the stack
+		// return visited nodes (in the order they were visited)
+		let visited = {};
+		let result = [];
+		let stack = [];
+		stack.push(vertex);
+		visited[vertex] = true;
+		while (stack.length !== 0) {
+			console.log(stack);
+			let current_vertex = stack.pop();
+			result.push(current_vertex);
+			for (let i = 0; i < this.adjacencyList[current_vertex].length; i++) {
+				let neighbor = this.adjacencyList[current_vertex][i];
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					stack.push(neighbor);
+				}
+			}
+		}
+		return result;
+	}
+
+	// Breadth First Traversal Iterative; USE A QUEUE!
+	// visit all neighbors at a given depth/height (number of edges from the source node)
+	breadth_first_traversal_iterative(vertex) {
+		// we queue the vertex into queue
+		// while queue is not empty
+		//		dequeue off the queue and mark as visited
+		//		queue the dequeued vertex's neighbors into the queue
+		// return visited nodes (in the order they were visited)
+		let visited = {};
+		let result = [];
+		let queue = [];
+		queue.push(vertex);
+		visited[vertex] = true;
+		while (queue.length !== 0) {
+			console.log(queue);
+			let current_vertex = queue.shift();
+			result.push(current_vertex);
+			for (let i = 0; i < this.adjacencyList[current_vertex].length; i++) {
+				let neighbor = this.adjacencyList[current_vertex][i];
+				if (!visited[neighbor]) {
+					visited[neighbor] = true;
+					queue.push(neighbor);
+				}
+			}
+		}
+		return result;
+	}
 }
+
+let g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A","B");
+g.addEdge("A","C");
+g.addEdge("B","D");
+g.addEdge("C","E");
+g.addEdge("D","E");
+g.addEdge("D","F");
+g.addEdge("E","F");
+
+console.log(g.depth_first_traversal_recursive("A"));
+console.log(g.depth_first_traversal_iterative("A"));
+console.log(g.breadth_first_traversal_iterative("A"));
