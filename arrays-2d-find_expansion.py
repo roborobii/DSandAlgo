e91adcdbfde10c6ce25767c5fa88e95679c0b78b
Robin Cristobal <racristob@gmail.com>
2019-04-30 09:13:46 -0700

    2d array find_expansion, recursive -> queue solution

--- arrays-2d-find_expansion.py
+++ arrays-2d-find_expansion.py
@@ -1,12 +1,12 @@
-def click(matrix, i, j): # O(rows*cols) ==~ O(N^2) algorithm
+def click_recursive(matrix, i, j): # recursive, O(rows*cols) ==~ O(N^2) algorithm
 	if matrix[i][j] == 0:
 		matrix[i][j] = -2
 		zero_neighbors = get_zero_neighbors(matrix, i, j)
-		for neighbor in zero_neighbors:
-			matrix = click(matrix, neighbor[0], neighbor[1])
+		for zero_neighbor in zero_neighbors:
+			matrix = click_recursive(matrix, zero_neighbor[0], zero_neighbor[1])
 	return matrix
 
-def get_zero_neighbors(matrix, i, j):
+def get_zero_neighbors(matrix, i, j): # O(3*3) every time -> O(1)
 	neighbors = []
 	rows = len(matrix)
 	cols = len(matrix[0])
@@ -16,4 +16,21 @@ def get_zero_neighbors(matrix, i, j):
 				neighbors.append([x,y])
 	return neighbors
 
-print(click([[0,0,0,0,0],[0,1,1,1,0],[0,1,-1,1,0]], 0, 1))
+print(click_recursive([[0,0,0,0,0],[0,1,1,1,0],[0,1,-1,1,0]], 0, 1))
+
+def click_queue(matrix, i, j): # non-recursive, O(rows*cols) ==~ O(N^2) algorithm
+	queue = []
+	if matrix[i][j] == 0:
+		matrix[i][j] = -2
+		queue.append((i,j))
+	else:
+		return matrix
+	while (len(queue) > 0):
+		i,j = queue.pop(0)
+		zero_neighbors = get_zero_neighbors(matrix, i, j) # O(1)
+		for zero_neighbor in zero_neighbors:
+			matrix[zero_neighbor[0]][zero_neighbor[1]] = -2
+			queue.append((zero_neighbor[0], zero_neighbor[1]))
+	return matrix
+
+print(click_queue([[0,0,0,0,0],[0,1,1,1,0],[0,1,-1,1,0]], 0, 1))
